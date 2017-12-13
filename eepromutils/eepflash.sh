9a96ddf1dc2929d1a4e5718c67e0a4b7800ac2cb
Nicola Lunghi <nicola.lunghi@emutex.com>
2017-12-13 11:09:00 +0000

    Added the possibility to select eeprom address and eeprom bus.

--- eepromutils/eepflash.sh
+++ eepromutils/eepflash.sh
@@ -3,6 +3,8 @@
 MODE="NOT_SET"
 FILE="NOT_SET"
 TYPE="NOT_SET"
+BUS="NOT_SET"
+ADDR="NOT_SET"
 
 usage()
 {
@@ -13,6 +15,8 @@ usage()
 	echo "	-r --read: read .eep from the eeprom"
 	echo "	-w --write: write .eep to the eeprom"
 	echo "	-f=file_name --file=file_name: binary .eep file to read to/from"
+	echo "	-d= --device= i2c bus number (ex if the eeprom is on i2c-0 set -d=0)"
+	echo "	-a= --address= i2c eeprom address"
 	echo "	-t=eeprom_type --type=eeprom_type: eeprom type to use"
 	echo "		We support the following eeprom types:"
 	echo "		-24c32"
@@ -21,6 +25,10 @@ usage()
 	echo "		-24c256"
 	echo "		-24c512"
 	echo "		-24c1024"
+	echo ""
+	echo "Example:"
+	echo "./eepflash -w -f=crex0.1.eep -t=24c32 -d=1 -a=57"
+	echo "./eepflash -r -f=dump.eep -t=24c32 -d=1 -a=57"
 	echo ""
 }
 
@@ -52,6 +60,12 @@ while [ "$1" != "" ]; do
 				exit 1
 			fi
 			;;
+		-d | --device)
+			BUS=$VALUE
+			;;
+		-a | --address)
+			ADDR=$VALUE
+			;;
 		-f | --file)
 			FILE=$VALUE
 			;;
@@ -75,7 +89,7 @@ elif [ "$TYPE" = "NOT_SET" ]; then
 	exit 1
 fi
 
-echo "This will attempt to talk to an eeprom at i2c address 0x50. Make sure there is an eeprom at this address."
+echo "This will attempt to talk to an eeprom at i2c address 0x$ADDR on bus $BUS. Make sure there is an eeprom at this address."
 echo "This script comes with ABSOLUTELY no warranty. Continue only if you know what you are doing."
 
 while true; do
@@ -88,24 +102,32 @@ while true; do
 done
 
 modprobe i2c_dev
-if [ -e "/dev/i2c-0" ]; then
-	BUS=0
-elif [ -e "/dev/i2c-3" ]; then
-	BUS=3
-else
-	dtoverlay i2c-gpio i2c_gpio_sda=0 i2c_gpio_scl=1
-	rc=$?
-	if [ $rc != 0 ]; then
-		echo "Loading of i2c-gpio dtoverlay failed. Do an rpi-update (and maybe apt-get update; apt-get upgrade)."
-		exit $rc
-	fi
-	if [ -e "/dev/i2c-3" ]; then
+if [ "$BUS" = "NOT_SET" ]; then
+	if [ -e "/dev/i2c-0" ]; then
+		BUS=0
+	elif [ -e "/dev/i2c-3" ]; then
 		BUS=3
 	else
-		echo "Expected I2C bus (i2c-3) not found."
+		dtoverlay i2c-gpio i2c_gpio_sda=0 i2c_gpio_scl=1
+		rc=$?
+		if [ $rc != 0 ]; then
+			echo "Loading of i2c-gpio dtoverlay failed. Do an rpi-update (and maybe apt-get update; apt-get upgrade)."
+			exit $rc
+		fi
+		if [ -e "/dev/i2c-3" ]; then
+			BUS=3
+		else
+			echo "Expected I2C bus (i2c-3) not found."
+		fi
 	fi
 fi
+
+if [ "$ADDR" = "NOT_SET" ]; then
+	ADDR=50
+fi
+
 modprobe at24
+
 rc=$?
 if [ $rc != 0 ]; then
 	echo "Modprobe of at24 failed. Do an rpi-update."
@@ -114,20 +136,19 @@ fi
 
 SYS=/sys/class/i2c-adapter/i2c-$BUS
 
-if [ ! -d "$SYS/$BUS-0050" ]; then
-	echo "$TYPE 0x50" > $SYS/new_device
+if [ ! -d "$SYS/$BUS-00$ADDR" ]; then
+	echo "$TYPE 0x$ADDR" > $SYS/new_device
 fi
-
 
 if [ "$MODE" = "write" ]
  then
 	echo "Writing..."
-	dd if=$FILE of=$SYS/$BUS-0050/eeprom
+	dd if=$FILE of=$SYS/$BUS-00$ADDR/eeprom status=progress
 	rc=$?
 elif [ "$MODE" = "read" ]
  then
 	echo "Reading..."
-	dd if=$SYS/$BUS-0050/eeprom of=$FILE
+	dd if=$SYS/$BUS-00$ADDR/eeprom of=$FILE status=progress
 	rc=$?
 fi
 
@@ -137,3 +158,7 @@ if [ $rc != 0 ]; then
 else
 	echo "Done."
 fi
+
+echo "Closing EEPROM Device."
+echo "0x$ADDR" > $SYS/delete_device
+
