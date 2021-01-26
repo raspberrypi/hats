0a873590105d72aed2752624122719b8d69e59f9
Philipp Rosenberger <p.rosenberger@kunbus.com>
2021-01-26 10:11:40 +0100

    eeptypes.h: cleanup formating
    - fix indention
    - move brace of the function to a new line
    - move tailing comment over the code line
    
    Signed-off-by: Philipp Rosenberger <p.rosenberger@kunbus.com>

--- eepromutils/eeptypes.h
+++ eepromutils/eeptypes.h
@@ -1,26 +1,26 @@
 #include <stdint.h>
 
 /* Atom types */
-#define ATOM_INVALID_TYPE 	0x0000
-#define ATOM_VENDOR_TYPE 0x0001
-#define ATOM_GPIO_TYPE	0x0002
-#define ATOM_DT_TYPE 		0x0003
-#define ATOM_CUSTOM_TYPE		0x0004
-#define ATOM_HINVALID_TYPE	0xffff
+#define ATOM_INVALID_TYPE   0x0000
+#define ATOM_VENDOR_TYPE    0x0001
+#define ATOM_GPIO_TYPE      0x0002
+#define ATOM_DT_TYPE        0x0003
+#define ATOM_CUSTOM_TYPE    0x0004
+#define ATOM_HINVALID_TYPE  0xffff
 
-#define ATOM_VENDOR_NUM 0x0000
-#define ATOM_GPIO_NUM 0x0001
-#define ATOM_DT_NUM 0x0002
+#define ATOM_VENDOR_NUM     0x0000
+#define ATOM_GPIO_NUM       0x0001
+#define ATOM_DT_NUM         0x0002
 
 //minimal sizes of data structures
 #define HEADER_SIZE 12
-#define ATOM_SIZE 10
+#define ATOM_SIZE   10
 #define VENDOR_SIZE 22
-#define GPIO_SIZE 30
-#define CRC_SIZE 2
+#define GPIO_SIZE   30
+#define CRC_SIZE     2
 
-#define GPIO_MIN 2
-#define GPIO_COUNT 28
+#define GPIO_MIN     2
+#define GPIO_COUNT  28
 
 #define FORMAT_VERSION 0x01
 
@@ -68,55 +68,54 @@ struct gpio_map_d {
 	unsigned char pins[GPIO_COUNT];
 };
 
-
-uint16_t getcrc(char* data, unsigned int size) {
-	
+uint16_t getcrc(char* data, unsigned int size)
+{
 	uint16_t out = 0;
-    int bits_read = 0, bit_flag;
-	
-    /* Sanity check: */
-    if((data == NULL) || size==0)
-        return 0;
+	int bits_read = 0, bit_flag;
 
-    while(size > 0)
-    {
-        bit_flag = out >> 15;
+	/* Sanity check: */
+	if((data == NULL) || size == 0)
+		return 0;
 
-        /* Get next bit: */
-        out <<= 1;
-        out |= (*data >> bits_read) & 1; // item a) work from the least significant bits
+	while(size > 0)
+	{
+		bit_flag = out >> 15;
 
-        /* Increment bit counter: */
-        bits_read++;
-        if(bits_read > 7)
-        {
+		/* Get next bit: */
+		out <<= 1;
+		// item a) work from the least significant bits
+		out |= (*data >> bits_read) & 1;
+
+		/* Increment bit counter: */
+		bits_read++;
+		if(bits_read > 7)
+		{
 			bits_read = 0;
-            data++;
-            size--;
-        }
+			data++;
+			size--;
+		}
 
-        /* Cycle check: */
-        if(bit_flag)
-            out ^= CRC16;
+		/* Cycle check: */
+		if(bit_flag)
+			out ^= CRC16;
+	}
 
-    }
+	// item b) "push out" the last 16 bits
+	int i;
+	for (i = 0; i < 16; ++i) {
+		bit_flag = out >> 15;
+		out <<= 1;
+		if(bit_flag)
+			out ^= CRC16;
+	}
 
-    // item b) "push out" the last 16 bits
-    int i;
-    for (i = 0; i < 16; ++i) {
-        bit_flag = out >> 15;
-        out <<= 1;
-        if(bit_flag)
-            out ^= CRC16;
-    }
+	// item c) reverse the bits
+	uint16_t crc = 0;
+	i = 0x8000;
+	int j = 0x0001;
+	for (; i != 0; i >>=1, j <<= 1) {
+		if (i & out) crc |= j;
+	}
 
-    // item c) reverse the bits
-    uint16_t crc = 0;
-    i = 0x8000;
-    int j = 0x0001;
-    for (; i != 0; i >>=1, j <<= 1) {
-        if (i & out) crc |= j;
-    }
-	
-    return crc;
+	return crc;
 }
